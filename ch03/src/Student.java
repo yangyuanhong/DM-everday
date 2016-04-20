a12ed0cab51cfaf9ec60e06431f7ce2960764331
yangyuanhong <1365212063@qq.com>
2016-04-20 21:37:33 +0800

    the third day

---
+++ ch03/src/Student.java
@@ -0,0 +1,17 @@
+package com.yyh.ch03;
+//**
+  
+public class Student
+{
+	public String name;
+	private double weight;
+    
+	public Student(){
+	}
+	public void sayHello(){
+		System.out.println(name+"shuo hello");
+}
+public int add(int a,int b){
+		return a+b;
+	}
+}
---
+++ ch03/src/Test.java
@@ -0,0 +1,10 @@
+package com.yyh.ch03;
+
+public class Test{
+	public static void main(String[] args){
+		Student stu;
+		stu=new Student();
+		stu.name="ÕÅÈı";
+		stu.sayHello();
+	}
+}
