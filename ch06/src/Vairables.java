fc1d27614f20d2f49b08fd12a1bd20ccdf34a47e
yangyuanhong <1365212063@qq.com>
2016-04-25 21:35:35 +0800

    第六课时

---
+++ ch06/src/Vairables.java
@@ -0,0 +1,25 @@
+package com.yyh.ch06;
+
+public class Vairables
+{ 
+	private int age;
+	double arry;
+	public static void main(String[] args)
+		{
+		Vairables var=new Vairables();
+		var.test1();
+		var.test2();
+	
+    public  void test1(){
+		int a=1;
+		System.out.println(a);
+		System.out.println(age);
+			
+		}
+	public void test2()
+		{
+         System.out.println(age);
+
+	    }
+       }
+}
